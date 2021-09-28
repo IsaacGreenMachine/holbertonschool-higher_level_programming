dfb7c427a949f2863213d6c07c4c5e6f4838cd9c
Isaac <zuderg@gmail.com>
2021-09-28 16:37:56 -0500

    added format to print

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -3,6 +3,6 @@ n = 1
 for i in range(10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print(i, j, sep="", end=", ")
+            print("{}{}".format(i, j), end=", ")
         else:
-            print(i, j, sep="")
+            print("{}{}".format(i, j))
