40d80b692feafbed21182fd91f40034bba49c17c
Isaac <zuderg@gmail.com>
2021-09-30 16:53:53 -0500

    fixed space issue

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print()
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+                print("{:d}".format(matrix[x][y]), end="")
+                if y != len(matrix[x])-1:
+                    print(" ", end="")
+                elif x != len(matrix)-1:
+                    print()
+    print()
