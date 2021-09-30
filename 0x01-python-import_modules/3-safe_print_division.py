75d4b68aa930be6c4ebeca7d4038d2d080997fc1
Isaac <zuderg@gmail.com>
2021-09-29 22:29:42 -0500

    fixed none -> None

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         final = a / b
     except ZeroDivisionError:
-        final = "none"
+        final = "None"
     finally:
         print("Inside result: {}".format(final))
         return final
