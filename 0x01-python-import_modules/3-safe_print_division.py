d6f5905e63e4f4661f37423db58dbbc6311a442b
Isaac <zuderg@gmail.com>
2021-09-29 22:09:43 -0500

    added README and scripts

---
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        final = a / b
+    except ZeroDivisionError:
+        final = "none"
+    finally:
+        print("Inside result: {}".format(final))
+        return final
