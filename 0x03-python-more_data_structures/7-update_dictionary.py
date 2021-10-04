773f66b3a39aeeedcad354de82b305039b1271fe
Isaac <zuderg@gmail.com>
2021-10-04 18:28:18 -0500

    fixed function

--- 0x03-python-more_data_structures/7-update_dictionary.py
+++ 0x03-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a = a_dictionary.copy()
-    a.setdefault(key, value)
-    return a
+    a_dictionary.update({key: value})
+    return a_dictionary
