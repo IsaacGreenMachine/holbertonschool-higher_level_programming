e076ce02a84ac6883adcfedf7cead9b25921469f
Isaac <zuderg@gmail.com>
2021-10-04 22:43:09 -0500

    did key instead of value

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     try:
-        return max(a_dictionary.values())
+        return(max(a_dictionary.items(), key=lambda x: x[1]))[0]
     except:
         return None
