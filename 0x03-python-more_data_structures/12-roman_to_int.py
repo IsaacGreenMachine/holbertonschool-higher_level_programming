5aaacfaeedb8271977f1be461a43d7b8feb6056d
Isaac <zuderg@gmail.com>
2021-10-05 09:38:17 -0700

    fixed fringe case None

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,8 @@ def roman_to_int(roman_string):
     a = r.get(roman_string[i])
     if i < len(roman_string) - 1:
         b = r.get(roman_string[i + 1])
+    if a == None or b == None:
+        return 0
     while i < len(roman_string) - 1:
         while i < len(roman_string) - 1 and a < b:
             sum -= r.get(roman_string[i])
