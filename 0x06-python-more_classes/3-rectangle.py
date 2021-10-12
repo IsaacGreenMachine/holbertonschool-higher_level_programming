db946015bf6844e98f662cf50744050d49c6d170
Isaac <zuderg@gmail.com>
2021-10-12 18:54:23 -0500

    fixed init issue

--- 0x06-python-more_classes/3-rectangle.py
+++ 0x06-python-more_classes/3-rectangle.py
@@ -6,8 +6,16 @@ class Rectangle:
     "This is the class for a rectangle"
 
     def __init__(self, width=0, height=0):
-        self.__height = height
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     @property
     def width(self):
