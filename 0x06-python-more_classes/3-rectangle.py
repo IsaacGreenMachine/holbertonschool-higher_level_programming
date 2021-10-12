10574d729546e2b26324fb4a37a0d5772dbda813
Isaac <zuderg@gmail.com>
2021-10-12 18:47:50 -0500

    added scripts and README

---
+++ 0x06-python-more_classes/3-rectangle.py
@@ -0,0 +1,51 @@
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((2 * self.__height) + (2 * self.__width))
+
+    def __str__(self):
+        squareStr = ""
+        if self.__height != 0 and self.__width != 0:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    squareStr += "#"
+                if i != (self.__height - 1):
+                    squareStr += "\n"
+        return squareStr
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
