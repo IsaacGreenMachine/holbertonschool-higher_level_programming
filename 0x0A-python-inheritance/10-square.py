99e3a64c20b73c7fa61aa57ec115e66c96d7eeed
Isaac <zuderg@gmail.com>
2021-10-13 23:12:44 -0500

    fixed correct Rectangle import

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 "This is the module for BaseGeometry"
-Rectangle = __import__('8-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     "raises Exception"
     def __init__(self, size):
         super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
