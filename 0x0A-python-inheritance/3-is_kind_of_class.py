64fca33f1171932bd1ae9981ea3df7febd642fd8
Isaac <zuderg@gmail.com>
2021-10-13 21:58:26 -0500

    added script

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"This is the module for isKindOfClass"
+
+
+def is_kind_of_class(obj, a_class):
+    "returns true or false based on class and obj"
+    return isinstance(obj, a_class)
