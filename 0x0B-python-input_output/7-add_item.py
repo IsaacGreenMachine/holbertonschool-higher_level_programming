35ee91421d73e94d54738a5066478e7df4934fc8
Isaac <zuderg@gmail.com>
2021-10-15 21:28:44 -0500

    fixed except clause

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     oldJson = load_from_json_file("add_item.json")
-except NameError:
+except FileNotFoundError:
     oldJson = []
 save_to_json_file(oldJson + sys.argv[1:], "add_item.json")
