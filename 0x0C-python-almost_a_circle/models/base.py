a878b5a21e2e0743da04d8fcb626cf735976185c
Isaac <zuderg@gmail.com>
2021-10-21 16:03:52 -0500

    added None handling

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        "init for Base. if given id, set to id, else set id to nth object"
+        """init for Base. if given id, set to id, else set id to nth object"""
         if id is not None:
             self.id = id
         else:
@@ -18,7 +18,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        "list of dictionaries -> json format"
+        """list of dictionaries -> json format"""
         if list_dictionaries is None or len(list_dictionaries) < 1:
             return "[]"
         else:
@@ -26,7 +26,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        "saves json format from list_objs to type.json file"
+        """saves json format from list_objs to type.json file"""
         name = cls.__name__ + ".json"
         with open(name, "w+") as f:
             if list_objs is None or len(list_objs) == 0:
@@ -39,19 +39,22 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        "returns obj from json_string"
-        return json.loads(json_string)
+        """returns obj from json_string"""
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        "creates a new object from given info"
+        """creates a new object from given info"""
         dum = cls(1, 1)
         dum.update(**dictionary)
         return dum
 
     @classmethod
     def load_from_file(cls):
-        "returns object from data stored in json format in type.json file"
+        """returns object from data stored in json format in type.json file"""
         filename = cls.__name__ + ".json"
         objlist = []
         try:
