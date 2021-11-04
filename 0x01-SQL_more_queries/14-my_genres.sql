0083a964e2ea0ef701f3c8e7bf2033331e3e2539
Isaac <zuderg@gmail.com>
2021-11-03 21:17:01 -0500

    fixed capitalizationg

--- 0x01-SQL_more_queries/14-my_genres.sql
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,6 @@ SELECT name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (SELECT id from tv_shows WHERE title = "Dexter")
+WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = "Dexter")
 ORDER BY name;
     
