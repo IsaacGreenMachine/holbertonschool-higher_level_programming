39c96248aced7f4a46cc6aac715976f8f74dc483
Isaac <zuderg@gmail.com>
2021-11-17 13:07:24 -0600

    added 10

---
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists all cities from the database hbtn_0e_4_usa by state"""
+
+if __name__ == "__main__":
+    from sqlalchemy.orm import session, sessionmaker
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instanceCount = 0
+    for instance in session.query(State).filter(State.name == sys.argv[4]):
+        print(instance.id)
+        instanceCount += 1
+    if instanceCount == 0:
+        print("Not found")
