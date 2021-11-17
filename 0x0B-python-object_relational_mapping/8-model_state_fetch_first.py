4dc62e8e38c7474d025367dde94f9f5fd5fc37ea
Isaac <zuderg@gmail.com>
2021-11-17 11:43:05 -0600

    adding 8

--- 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,18 +7,15 @@ if __name__ == "__main__":
     from model_state import State, Base
     from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for i in range(5):
-        try:
-            state = session.query(State)[i]
-            print(state.__dict__)
-            idnum = state.id
-            name = state.name
-            print("{}: {}".format(idnum, name))
-        except Exception:
-            print("Nothing")
+    try:
+        state = session.query(State)[0]
+        idnum = state.id
+        name = state.name
+        print("{}: {}".format(idnum, name))
+    except Exception:
+        print("Nothing")
