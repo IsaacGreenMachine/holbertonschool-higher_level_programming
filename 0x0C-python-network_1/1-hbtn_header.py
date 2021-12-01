4cfcae853da3996f4e64f3a0ca49b73d782bc2ef
Isaac <zuderg@gmail.com>
2021-12-01 15:08:01 -0600

    formatting and main guarding

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """takes URL, sends request to URL, displays value of X-Request-Id"""
-from sys import argv
-import urllib.request
-req = urllib.request.Request(argv[1])
-with urllib.request.urlopen(req) as response:
-    html = response.read()
-    print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    from sys import argv
+    import urllib.request
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(response.headers.get("X-Request-Id"))
--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-from urllib import parse, request
-import urllib
-url = argv[1]
-values = {"email": argv[2]}
-data = parse.urlencode(values)
-data = data.encode('ascii')
-req = request.Request(url, data)
-with request.urlopen(req) as response:
-    html = response.read()
-    print(html.decode('utf-8'))
+"""sends POST request to URL with email parameter, displays response body"""
+if __name__ == "__main__":
+    from sys import argv
+    from urllib import parse, request
+    import urllib
+    url = argv[1]
+    values = {"email": argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
