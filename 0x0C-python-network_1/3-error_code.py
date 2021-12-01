81c3cec80922d7e413f6ce77b87410a04a827c0c
Isaac <zuderg@gmail.com>
2021-12-01 15:27:06 -0600

    formatted d3

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""takes URL, sends request to URL, displays value of X-Request-Id"""
+"""sends request to URL and displays response with HTTPError handling"""
 if __name__ == "__main__":
-	from sys import argv
-	import urllib.request
-	req = urllib.request.Request(argv[1])
-	try:
-		with urllib.request.urlopen(req) as response:
-			html = response.read()
-			print(html.decode('utf-8'))
-	except Exception as ex:
-		print("Error code: {}".format(ex.code))
+    from sys import argv
+    import urllib.request
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except Exception as ex:
+        print("Error code: {}".format(ex.code))
