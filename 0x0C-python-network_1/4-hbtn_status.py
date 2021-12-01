8b77bfb1dcf30554ab619a87ef07360afad09a27
Isaac <zuderg@gmail.com>
2021-12-01 15:39:15 -0600

    added 4 formatting

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import requests
 r = requests.get('https://intranet.hbtn.io/status')
-print(type(r.text))
-print(r.text)
+print("Body response:")
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
