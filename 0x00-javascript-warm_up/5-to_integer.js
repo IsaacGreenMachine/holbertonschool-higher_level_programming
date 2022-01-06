90a8fbf0d1f446aebe267538a23417dc472daa2a
Isaac Green <zuderg@gmail.com>
2022-01-06 17:47:40 -0600

    playing with formatting

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[2])) {
-  console.log('Not a number');
+if (parseInt(process.argv[2])) {
+  console.log('My Number: ' + parseInt(process.argv[2]));
 } else {
-  console.log('My Number:', parseInt(process.argv[2]));
+  console.log('Not a number');
 }
