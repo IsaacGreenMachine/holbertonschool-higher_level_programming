9eb1819d157efc96cb1462c583652ddb843ef2ec
Isaac Green <zuderg@gmail.com>
2022-01-07 16:26:39 -0600

    removed extra

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -8,9 +8,6 @@ class Square extends FiveSquare {
   }
 
   charPrint (c = 'X') {
-    if (!c) {
-      c = 'X';
-    }
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write(c);
