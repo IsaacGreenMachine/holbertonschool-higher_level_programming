0039a6f8846ee0e8416ee3e5f2820814152295f5
Isaac Green <zuderg@gmail.com>
2022-01-07 16:23:07 -0600

    removed default value

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,10 @@ class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c = 'X') {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write(c);
