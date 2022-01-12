f306939cb6f095aadc338f500adc02b229cfd836
Isaac Green <zuderg@gmail.com>
2022-01-11 18:53:14 -0600

    fixed parse

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,7 @@ request(process.argv[2], function (error, response, body) {
   const moviesList = JSON.parse(body).results;
   for (let i = 0; i < moviesList.length; i++) {
     for (let j = 0; j < moviesList[i].characters.length; j++) {
-      if (moviesList[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+      if (moviesList[i].characters[j].includes('18')) {
         n++;
       }
     }
