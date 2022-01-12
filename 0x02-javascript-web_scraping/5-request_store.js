e574304fbd86697a0a185e9fb2c6f54cc5de5aa2
Isaac Green <zuderg@gmail.com>
2022-01-11 19:07:39 -0600

    added task 5

---
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  fs.writeFile(process.argv[3], body, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
